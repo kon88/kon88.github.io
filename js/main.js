3aaf83ccf80cd8b06065dd6be2f231b24d3a3684
Kon Kim <kon.kim@outlook.com>
2016-07-11 14:39:10 -0400

    added recent proj anim

--- js/main.js
+++ js/main.js
@@ -5,13 +5,17 @@ $(".wrapper #myMessage p").lettering();
         
     var text1 = $('.wrapper #myMessage h2');
     var text2 = $('.wrapper #myMessage h1');
+    var btn1 = $('.wrapper #myLinks .btn-1');
+    var btn2 = $('.wrapper #myLinks .btn-2');
+    var box1 = $('.latestProj');
+    var par1 = $('.wrapper #myMessage p');
     
     var tl1 = new TimelineMax({delay: .75});
     
-    tl1.from(text1, .7, {autoAlpha: 0, y: -50})
-    .from(text2, .7, {autoAlpha:0},"+=.3")
-    .call(function(){
-       TweenMax.set(".wrapper #myMessage p", {autoAlpha: 1});
+    tl1.from(text1, .7, {autoAlpha: 0, y: -50}).
+    from(text2, .7, {autoAlpha:0},"+=.2").
+    call(function(){
+       TweenMax.set(par1, {autoAlpha: 1});
         $('.wrapper #myMessage [class^="char"]').each(function(i) {
         TweenMax.from($(this), 1, {
             autoAlpha: 0,
@@ -19,18 +23,26 @@ $(".wrapper #myMessage p").lettering();
             y: random(-300, 300),
             z: random(-300, 300),
             scale: 1.5,
-            delay: i * .02
+            delay: i * .01
             });
     });
 
-  function random(min, max) {
+    function random(min, max) {
     return (Math.random() * (max - min)) + min;
   }
+
+var tl2 = new TimelineLite({delay: 1.75});
+    tl2.from(btn1, 1, {autoAlpha: 0, x: -25}, "+=.3").
+    from(btn2, 1, {autoAlpha: 0, x: 25}, "-=.9").
+    to(box1, 1, {autoAlpha: 1, width: "100%"}).
+    to(box1, 1, {backgroundColor: "rgba(238,0,0,1)"}, "+=.2");
+
+  
 });
         
 
 $('#mySlider').slick({
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 4000,
     dots: true,
     infinite: true,
